954b0a7928c8880a5fc95ba171a30e080b3e4086
wonvoc96 <83491014+wonvoc96@users.noreply.github.com>
2024-08-07 17:33:03 +0900

    panoDefault

---
+++ data.js
@@ -0,0 +1,60 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-testbed",
+      "name": "TESTBED",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.5653987281424273,
+          "pitch": -0.0026105623035235226,
+          "rotation": 0,
+          "target": "0-testbed"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.7935556882622912,
+          "pitch": -0.043600329411017213,
+          "title": "모돈자동급이기",
+          "text": "(이미지)<div><br></div><div>-</div><div><br></div><div>홈페이지1</div><div><br></div><div>제품소개1</div><div>-제품설명1</div><div><br></div><div>제품소개2</div><div>-제품설명2</div><div><br></div><div>제품소개3</div><div>-제품설명3</div>"
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
